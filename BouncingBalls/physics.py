84228a1da618c06c1b94903098ddd4fe5b194489
ChristianCompiles <christian.penick@wsu.edu>
2024-06-04 21:33:38 -0700

    added acceleration

--- BouncingBalls/physics.py
+++ BouncingBalls/physics.py
@@ -1,10 +1,10 @@
 from vpython import *
-
+# https://www.glowscript.org/docs/VPythonDocs/index.html
 scene.width = scene.height = 600
 scene.background = color.white
 scene.range = 1.3
 N = 1
-scene.title = f'A {N}-element points object with random radii and colors'
+scene.title = f'A {N}-ball bouncing on floor sim'
 # Display frames per second and render time:
 scene.append_to_title("<div id='fps'/>")
 
@@ -28,13 +28,29 @@ Touch screen: pinch/extend to zoom, swipe or two-finger rotate.""")
 
 ball = sphere (color = color.green, radius = 0.4, make_trail=False, retain=200)
 ball.mass = 1.0
-ball.p = vector (0, -0.23, 0)
-
-sim_floor = 4.0
+iv_y = -0.23 # initial y velocity just barely downwards
+ball.pos = vector (0.0, 1, 0)
+ball.yvel = 0
+sim_floor = 0.0
+ball.y_acl = -9.8
 
 dt = 0.3
+cf = 0.4 # coefficient restitution
+g = -9.8
+fps = 60
+
+# 1 frame = 1/60 sec
+
+tpf = 15/fps # time per frame
+
 while True:
-    rate(30)
-    ball.pos = ball.pos + (ball.p/ball.mass)*dt
+    rate(fps)
+    
+    ball.pos.y = ball.pos.y + ball.yvel*(tpf) + 0.5*g*(tpf)**2
+    ball.yvel = ball.yvel + g*(tpf)
+    print(ball.yvel)
+    #ball.pos.y = ball.pos.y + (ball.pos.y/ball.mass)*dt
     if (ball.pos.y <= sim_floor): # going to hit floor
-        ball.p.y = -ball.p.y
+        print("hit floor")
+        ball.yvel = -ball.yvel * cf
+        ball.y_acl = -ball.y_acl * cf
